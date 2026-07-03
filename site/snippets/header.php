ff10004257f775cda730994fb608235b4fb47507
Giulia Polenta <giuliapolenta@gmail.com>
2026-07-03 11:06:03 +0200

    header has auto width

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,5 +1,4 @@
 <header class="header">
-    <!-- <h1>You're logged in as that one girl with pink hair</h1> -->
     <nav class="nav">
         <a class="nav-label" href="/">Info</a>
         <a class="nav-label" href="/">About</a>
